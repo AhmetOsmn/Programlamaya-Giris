06551d11b4689d1e549c7c356db90ca13062fab3
AhmtOsmn <44196434+AhmtOsmn@users.noreply.github.com>
2020-06-16 20:20:15 +0300

    Delete 57.kalan_borc_hesapla.py

--- 57.kalan_borc_hesapla.py
+++
@@ -1,8 +0,0 @@
-kredi = float(input("Çekilen kredi: "))
-ay = int(input("Kaç ay: "))
-
-odenecek = kredi + (kredi*9)/100
-aylık = odenecek/ay
-for i in range(3):
-    odenecek -=  aylık
-    print("Kalan Borcunuz: ",odenecek)
