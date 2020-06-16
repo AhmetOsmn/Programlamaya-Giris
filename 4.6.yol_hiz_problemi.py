35bb861a0a6a89876e7360096ab3870990b78911
AhmtOsmn <44196434+AhmtOsmn@users.noreply.github.com>
2020-06-16 20:18:34 +0300

    Delete 4.6.yol_hiz_problemi.py

--- 4.6.yol_hiz_problemi.py
+++
@@ -1,6 +0,0 @@
-mesafe = float(input("Mesafe (km): "))
-ortalama_hiz = float(input("Ortalama Hiz (km/saat): "))
-
-sure = mesafe / ortalama_hiz
-sure = round(sure,2)
-print("Varis suresi: {}'saattir.".format(sure))
