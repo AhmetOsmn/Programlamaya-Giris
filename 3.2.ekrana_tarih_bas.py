6f846b806f89df8e9ffa8df01eb1ed3274550169
AhmtOsmn <44196434+AhmtOsmn@users.noreply.github.com>
2020-06-16 20:16:37 +0300

    Delete 3.2.ekrana_tarih_bas.py

--- 3.2.ekrana_tarih_bas.py
+++
@@ -1,9 +0,0 @@
-gun = input("Gun: ")
-ay = input("Ay: ")
-yil = input("Yil: ")
-
-
-
-print("Gun Ay Yil: {}.{}.{}".format(gun,ay,yil))
-print("Ay Gun Yil: {}.{}.{}".format(ay,ay,yil))
-print("Yil Gun Ay: {}.{}.{}".format(yil,gun,ay))
