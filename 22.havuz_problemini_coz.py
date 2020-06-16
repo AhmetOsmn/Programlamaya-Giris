97e8c133ab6975e17555da5ceff2891ff9e1f541
AhmtOsmn <44196434+AhmtOsmn@users.noreply.github.com>
2020-06-16 20:14:22 +0300

    Delete 22.havuz_problemini_coz.py

--- 22.havuz_problemini_coz.py
+++
@@ -1,8 +0,0 @@
-a = float(input("a muslugu kac saatte doldurmakta(l/saat): "))
-b = float(input("b muslugu kac saatte doldurmakta(l/saat): "))
-c = float(input("c muslugu kac saatte doldurmakta(l/saat): "))
-
-havuz = float(input("Havuz hacmi(l): "))
-
-sure = (a+b+c)/havuz
-print("Havuz {}'saatte dolar.".format(sure))
